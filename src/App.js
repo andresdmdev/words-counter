201a2b92e730815f8ab63c23b486984bd441fb5d
Andres Marquez <andresmftech5@gmail.com>
2022-04-18 16:33:39 -0500

    Modificar Archivo App.js y App.css
    -App.js-Se agregaron dos componentes  y se completo la logica. -App.css-Se agregaron los estilos al archivo principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import InputTextarea from './components/input';
+import Show from './components/show';
+import { useState } from 'react';
 
 function App() {
+
+  const [text, setText] = useState(String());
+
+  const input = input => {
+    setText(input.trim());
+  }
+
   return (
     <div className="App">
-      
+      <h1>Word Counter</h1>
+      <h3>Enter some text below:</h3>
+      <InputTextarea 
+        onChange={input} 
+      />
+      <Show 
+        text={text} 
+      />
     </div>
   );
 }
